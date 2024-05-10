2b9b714a787cec2f655d48bfa2e31baaf7cd8964
Angela Chen <achen313@calpoly.edu>
2024-05-10 16:28:30 -0700

    edit user to match new database

--- src/api/user.py
+++ src/api/user.py
@@ -19,9 +19,9 @@ def create_user(name: str):
     # create a user
     with db.engine.begin() as connection:
         try:
-            id = connection.execute(sqlalchemy.text("INSERT INTO users (name, gold) VALUES (:name, 200) RETURNING user_id"), [{"name": name}])
+            id = connection.execute(sqlalchemy.text("INSERT INTO users (name) VALUES (:name) RETURNING user_id"), [{"name": name}])
             user_id = id.fetchone()[0]
-            connection.execute(sqlalchemy.text("INSERT INTO transactions (user_id, user_name, gold, description) VALUES (:user_id, :user_name, :money, :description)"), [{"user_id": user_id, "user_name": name, "money": 200, "description": "create new user"}])
+            connection.execute(sqlalchemy.text("INSERT INTO transactions (user_id, user_name, gold, description) VALUES (:user_id, :user_name, :money, :description)"), [{"user_id": user_id, "user_name": name, "money": 200, "description": "starting gold"}])
             print(f"user_id: {user_id}")
         except IntegrityError:
             return "INTEGRITY ERROR!"
