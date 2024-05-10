7c472d4a4a6f3abf891ba0ef1585dc993072af5e
Angela Chen <achen313@calpoly.edu>
2024-05-10 13:40:21 -0700

    new database design

--- src/api/animal.py
+++ src/api/animal.py
@@ -16,21 +16,41 @@ def create_animal():
     return "OK"
 
 @router.post("/buy-animal/{animal_name}")
-def buy_animal(animal_id: int, animal_name: str, user_id: int, user_name: str):
+def buy_animal(animal_id: int, animal_name: str, user_id: int):
     status = False
     with db.engine.begin() as connection:
         try:
-            user = connection.execute(sqlalchemy.text("SELECT gold, name, user_id FROM users WHERE user_id = :user_id"), [{"user_id": user_id}])
-            price = connection.execute(sqlalchemy.text("SELECT price FROM animals WHERE animal_id = :animal_id AND name = :animal_name"), [{"animal_id": animal_id, "animal_name": animal_name}])
-            user = user.fetchone()
-            price = price.fetchone()[0]
-            # if user can afford buying that animal
-            if user[0] >= price:
-                description = 'bought ' + animal_name
-                connection.execute(sqlalchemy.text("INSERT INTO transactions (user_id, user_name, gold, description) VALUES (:user_id, :user_name, -:money, :description)"), [{"user_id": user[2], "user_name": user[1], "money": price, "description": description}])
-                connection.execute(sqlalchemy.text("UPDATE animals SET availability = FALSE WHERE name = :animal_name"), [{"animal_name": animal_name}])
-                connection.execute(sqlalchemy.text("UPDATE users SET animal_id = :animal_id WHERE user_id = :user_id AND name = :user_name"), [{"animal_id": animal_id, "user_id": user_id, "user_name": user_name}])
-                status = True
+            # find user's gold
+            user = connection.executesqlalchemy.text("SELECT SUM(gold) AS gold FROM transactions WHERE user_id = :user_id", [{"user_id": user_id}]).one()
+            
+            # find price of animal
+            animal = connection.executesqlalchemy.text("SELECT user_id, price FROM animals WHERE animal_id = :animal_id)", [{"animal_id": animal_id}]).one()
+
+            if user.gold > animal.price:
+                print("user can afford animal")
+
+
+                # check if unowned
+                if(animal.user_id is None):
+                    print("animal is available")
+                    # insert into transactions 
+                    connection.execute(sqlalchemy.text("INSERT INTO transactions (user_id, gold, desc) VALUES (:user_id, -:gold, :desc)"), [{"user_id": user[2], "gold": animal.price, "desc": "buy animal"}])
+
+                    # update animal to user link
+                    connection.execute(sqlalchemy.text("UPDATE users SET animal_id = :animal_id WHERE user_id = :user_id"), [{"animal_id": animal_id, "user_id": user_id}])
+                    connection.execute(sqlalchemy.text("UPDATE animals SET user_id = :user_id WHERE animal_id = :animal_id"), [{"user_id": user_id, "animal_id": animal_id}])
+
+            # user = connection.execute(sqlalchemy.text("SELECT gold, name, user_id FROM users WHERE user_id = :user_id"), [{"user_id": user_id}])
+            # price = connection.execute(sqlalchemy.text("SELECT price FROM animals WHERE animal_id = :animal_id AND name = :animal_name"), [{"animal_id": animal_id, "animal_name": animal_name}])
+            # user = user.fetchone()
+            # price = price.fetchone()[0]
+            # # if user can afford buying that animal
+            # if user[0] >= price:
+            #     description = 'bought ' + animal_name
+            #     connection.execute(sqlalchemy.text("INSERT INTO transactions (user_id, user_name, gold, description) VALUES (:user_id, :user_name, -:money, :description)"), [{"user_id": user[2], "user_name": user[1], "money": price, "description": description}])
+            #     connection.execute(sqlalchemy.text("UPDATE animals SET availability = FALSE WHERE name = :animal_name"), [{"animal_name": animal_name}])
+            #     connection.execute(sqlalchemy.text("UPDATE users SET animal_id = :animal_id WHERE user_id = :user_id AND name = :user_name"), [{"animal_id": animal_id, "user_id": user_id, "user_name": user_name}])
+            #     status = True
         except IntegrityError:
             return "INTEGRITY ERROR!"
     
