a65533024736fe06f0da959ee258c4de943e79ef
soramicha <schang7jcu@gmail.com>
2024-06-05 01:48:11 -0700

    changed back to actual database; deleted unnecessary code from animal.py

--- src/database.py
+++ src/database.py
@@ -7,5 +7,5 @@ def database_connection_url():
 
     return os.environ.get("POSTGRES_URI")
 
-#engine = create_engine(database_connection_url(), pool_pre_ping=True)
-engine = create_engine("postgresql://postgres:postgres@127.0.0.1:54322/postgres", pool_pre_ping=True)
+engine = create_engine(database_connection_url(), pool_pre_ping=True)
+#engine = create_engine("postgresql://postgres:postgres@127.0.0.1:54322/postgres", pool_pre_ping=True)
